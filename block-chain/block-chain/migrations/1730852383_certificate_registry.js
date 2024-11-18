24f70296b8fdf8255a198bd7ef1f89599345146e
Rodrigo Balestra Cortese <86007316+RoCortese@users.noreply.github.com>
2024-11-18 09:57:17 -0300

    Delete block-chain/block-chain directory

--- block-chain/block-chain/migrations/1730852383_certificate_registry.js
+++
@@ -1,6 +0,0 @@
-const CertificateRegistry = artifacts.require('../contracts/CertificateRegistry.sol');
-
-module.exports = async function (_deployer) {
-  await _deployer.deploy(CertificateRegistry);
-  await CertificateRegistry.deployed();
-};
